26654f37f6cb0aafe8599575a43fc79845331de6
Slava Pavlutin <sl.pavlutin@gmail.com>
2020-10-10 01:04:13 +0300

    use correct formatting for current time

--- lib/hooks/useCurrentTime.ts
+++ lib/hooks/useCurrentTime.ts
@@ -1,12 +1,17 @@
 import { format } from 'date-fns';
 import { useEffect, useState } from 'react';
 
+const FORMAT_STRING = 'HH:mm';
+
+/**
+ * Returns current time every minute
+ */
 export function useCurrentTime() {
-  let [time, setTime] = useState(format(new Date(), 'k:mm'));
+  let [time, setTime] = useState(format(new Date(), FORMAT_STRING));
 
   useEffect(() => {
     let id = setInterval(() => {
-      let currentTime = format(new Date(), 'k:mm');
+      let currentTime = format(new Date(), FORMAT_STRING);
       if (currentTime !== time) {
         setTime(currentTime);
       }
