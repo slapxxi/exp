8347fe794faa90967bac553d6a4047b7eb5ea43b
Slava Pavlutin <sl.pavlutin@gmail.com>
2020-10-14 00:08:51 +0300

    create tabs component

---
+++ components/Tabs.tsx
@@ -0,0 +1,105 @@
+import { css } from '@emotion/core';
+import { ThemedCSS } from '@self/lib/types';
+import { Children, cloneElement, createContext, isValidElement } from 'react';
+import tw from 'twin.macro';
+
+let tabsContext = createContext<{ activeTab: string }>({ activeTab: null });
+
+interface TabsProps {
+  value: number;
+  onChange: (index: number) => void;
+}
+
+const DIV = 3;
+
+export let Tabs: React.FC<TabsProps> = (props) => {
+  let { children, value, onChange } = props;
+  let count = Children.count(children);
+  let decoratorWidth = 100 / count / DIV;
+  let decoratorOffset = calcDecoratorOffset(decoratorWidth, count, value);
+
+  return (
+    <div
+      css={
+        ((theme) => css`
+          ${tw`flex relative shadow sticky top-0`}
+          background: ${theme.colors.bgItem};
+          min-height: 60px;
+
+          ::after {
+            ${tw`absolute bottom-0`}
+            content:  '';
+            height: 3px;
+            background: ${theme.colors.accent};
+            width: ${decoratorWidth}%;
+            transform: translate3d(${decoratorOffset}%, 0, 0);
+            transition: transform 0.3s;
+          }
+        `) as ThemedCSS
+      }
+    >
+      {Children.map(children, (c, i) => {
+        if (isValidElement(c)) {
+          return cloneElement(c, {
+            active: i === value,
+            onClick: () => {
+              onChange(i);
+              c.props.onClick?.();
+            },
+          });
+        }
+
+        return null;
+      })}
+    </div>
+  );
+};
+
+function calcDecoratorOffset(
+  decoratorWidth: number,
+  itemsCount: number,
+  currentIndex: number,
+): number {
+  let containerWidth = 100 / itemsCount;
+  let remainingSpace = containerWidth / decoratorWidth - 1;
+  let result =
+    (remainingSpace + currentIndex * (containerWidth / decoratorWidth) - remainingSpace / 2) * 100;
+  return result;
+}
+
+interface TabProps {
+  label: string;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+export let Tab: React.FC<TabProps> = (props) => {
+  let { active, label, onClick } = props;
+
+  return (
+    <div
+      css={(theme) => css`
+        ${tw`flex flex-1 items-center justify-center px-4 cursor-pointer`}
+        color: ${active ? theme.colors.accent : theme.colors.textItem};
+      `}
+      onClick={onClick}
+    >
+      {label}
+    </div>
+  );
+};
+
+interface TabPanelProps {
+  value: number;
+  index: number;
+}
+
+export let TabPanel: React.FC<TabPanelProps> = (props) => {
+  let { children, value, index } = props;
+
+  if (value === index) {
+    return <>{children}</>;
+  }
+
+  return null;
+};
--- pages/clients.tsx
+++ pages/clients.tsx
@@ -1,11 +1,12 @@
 import { css } from '@emotion/core';
 import styled from '@emotion/styled';
+import { Tab, TabPanel, Tabs } from '@self/components/Tabs';
 import { getOrders } from '@self/lib/services/getOrders';
 import { Themed, ThemedCSS } from '@self/lib/types';
 import { format } from 'date-fns';
 import { NextPage } from 'next';
 import Head from 'next/head';
-import React, { Fragment, SVGProps } from 'react';
+import React, { Fragment, SVGProps, useState } from 'react';
 import { Grid, PhoneCall, PhoneMissed, PhoneOff, Search } from 'react-feather';
 import { useQuery } from 'react-query';
 import tw from 'twin.macro';
@@ -16,6 +17,7 @@ interface Props {
 
 let ClientsPage: NextPage<Props> = (props) => {
   let { initialData } = props;
+  let [activeTab, setActiveTab] = useState(1);
   let { status, data } = useQuery('ordersData', getOrders, { initialData: initialData });
 
   return (
@@ -24,144 +26,163 @@ let ClientsPage: NextPage<Props> = (props) => {
         <title>Clients | Dashboard</title>
       </Head>
 
-      <div
-        css={css`
-          ${tw`p-4`}
-        `}
-      >
+      <div>
+        <Tabs value={activeTab} onChange={(value) => setActiveTab(value)}>
+          <Tab label="Requests"></Tab>
+          <Tab label="Property"></Tab>
+          <Tab label="Clients"></Tab>
+          <Tab label="Deals"></Tab>
+          <Tab label="Deals"></Tab>
+        </Tabs>
+
         <div
-          css={
-            ((theme) => css`
-              ${tw`flex rounded shadow my-4`}
-              min-height: 48px;
-              background: ${theme.colors.bgItem};
-              color: ${theme.colors.textItem};
-
-              > * {
-                border-right: 1px solid ${theme.colors.bgContent};
-
-                &:last-child {
-                  border: 0;
-                }
-              }
-            `) as ThemedCSS
-          }
+          css={css`
+            ${tw`p-4`}
+          `}
         >
-          <ToolbarItem>
-            <label
-              htmlFor="search-orders"
-              css={css`
-                align-self: center;
-              `}
-            >
-              Search
-            </label>
-          </ToolbarItem>
-          <ToolbarItem
-            noPadding
-            css={css`
-              ${tw`flex-1`}
-            `}
-          >
-            <input
-              id="search-orders"
-              type="search"
-              placeholder="Search..."
+          <TabPanel value={activeTab} index={0}>
+            <div
               css={
                 ((theme) => css`
-                  ${tw`flex-1 px-4`}
+                  ${tw`flex rounded shadow my-4`}
+                  min-height: 48px;
                   background: ${theme.colors.bgItem};
                   color: ${theme.colors.textItem};
 
-                  ::placeholder {
-                    color: ${theme.colors.textContent};
+                  > * {
+                    border-right: 1px solid ${theme.colors.bgContent};
+
+                    &:last-child {
+                      border: 0;
+                    }
                   }
                 `) as ThemedCSS
               }
-            ></input>
-          </ToolbarItem>
-          <ToolbarItem noPadding>
-            <button
-              css={css`
-                ${tw`px-4`}
-              `}
             >
-              <Search strokeWidth="1.5"></Search>
-            </button>
-          </ToolbarItem>
-          <ToolbarItem noPadding>
-            <button
-              css={css`
-                ${tw`px-4 pr-2`}
-              `}
+              <ToolbarItem noPadding>
+                <label
+                  htmlFor="search-orders"
+                  css={css`
+                    ${tw`flex items-center px-4`}
+                  `}
+                >
+                  Search
+                </label>
+              </ToolbarItem>
+              <ToolbarItem
+                noPadding
+                css={css`
+                  ${tw`flex-1`}
+                `}
+              >
+                <input
+                  id="search-orders"
+                  type="search"
+                  placeholder="Search..."
+                  css={
+                    ((theme) => css`
+                      ${tw`flex-1 px-4`}
+                      background: ${theme.colors.bgItem};
+                      color: ${theme.colors.textItem};
+
+                      ::placeholder {
+                        color: ${theme.colors.textContent};
+                      }
+                    `) as ThemedCSS
+                  }
+                ></input>
+              </ToolbarItem>
+              <ToolbarItem noPadding>
+                <button
+                  css={css`
+                    ${tw`px-4`}
+                  `}
+                >
+                  <Search strokeWidth="1.5"></Search>
+                </button>
+              </ToolbarItem>
+              <ToolbarItem noPadding>
+                <button
+                  css={css`
+                    ${tw`px-4 pr-2`}
+                  `}
+                >
+                  <StackIcon strokeWidth="1.5"></StackIcon>
+                </button>
+                <button
+                  css={css`
+                    ${tw`px-4 pl-2`}
+                  `}
+                >
+                  <Grid strokeWidth="1.5"></Grid>
+                </button>
+              </ToolbarItem>
+            </div>
+
+            {/* Table */}
+            <table
+              css={(theme) =>
+                css`
+                  ${tw`w-full`}
+                  color: ${theme.colors.textTableHeader};
+                `
+              }
             >
-              <StackIcon strokeWidth="1.5"></StackIcon>
-            </button>
-            <button
-              css={css`
-                ${tw`px-4 pl-2`}
-              `}
-            >
-              <Grid strokeWidth="1.5"></Grid>
-            </button>
-          </ToolbarItem>
+              <thead>
+                <tr
+                  css={css`
+                    ${tw`shadow`}
+                  `}
+                >
+                  <HCell>Status</HCell>
+                  <HCell>Phone Number</HCell>
+                  <HCell>Date</HCell>
+                  <HCell>Curator</HCell>
+                  <HCell>Duration</HCell>
+                  <HCell colSpan={2}>Recording</HCell>
+                </tr>
+                <Spacer></Spacer>
+              </thead>
+              <tbody>
+                {status === 'success' &&
+                  data.map((item, i) => (
+                    <Fragment key={item.id}>
+                      <Row>
+                        <Cell
+                          title={item.status}
+                          css={css`
+                            text-align: center;
+                            vertical-align: middle;
+                          `}
+                        >
+                          {item.status === 'completed' && (
+                            <SuccessIcon as={PhoneCall}></SuccessIcon>
+                          )}
+                          {item.status === 'no-response' && (
+                            <ErrorIcon as={PhoneMissed}></ErrorIcon>
+                          )}
+                          {item.status === 'busy' && <ErrorIcon as={PhoneOff}></ErrorIcon>}
+                        </Cell>
+                        <Cell>{item.phoneNumber}</Cell>
+                        <Cell>{format(new Date(item.callDate), 'dd.MM.yyyy hh:mm:ss')}</Cell>
+                        <Cell>{item.curator}</Cell>
+                        <Cell>{item.duration ? ~~(item.duration / 60) : 'N/A'}</Cell>
+                        <Cell>
+                          <PlayIcon></PlayIcon>
+                        </Cell>
+                        <Cell>
+                          {item.status === 'completed' && <Button>Add</Button>}
+                          {item.status === 'no-response' && <Button>Call Back</Button>}
+                          {item.status === 'busy' && <Button>Call Back</Button>}
+                        </Cell>
+                      </Row>
+                      <Spacer key={i}></Spacer>
+                    </Fragment>
+                  ))}
+              </tbody>
+            </table>
+          </TabPanel>
         </div>
-        <table
-          css={(theme) =>
-            css`
-              ${tw`w-full`}
-              color: ${theme.colors.textTableHeader};
-            `
-          }
-        >
-          <thead
-            css={css`
-              ${tw`text-left`}
-            `}
-          >
-            <tr css={css``}>
-              <th>Status</th>
-              <th>Phone Number</th>
-              <th>Date</th>
-              <th>Curator</th>
-              <th>Duration</th>
-              <th colSpan={2}>Recording</th>
-            </tr>
-          </thead>
-          <tbody>
-            {status === 'success' &&
-              data.map((item, i) => (
-                <Fragment key={item.id}>
-                  <Row>
-                    <Cell
-                      title={item.status}
-                      css={css`
-                        text-align: center;
-                        vertical-align: middle;
-                      `}
-                    >
-                      {item.status === 'completed' && <SuccessIcon as={PhoneCall}></SuccessIcon>}
-                      {item.status === 'no-response' && <ErrorIcon as={PhoneMissed}></ErrorIcon>}
-                      {item.status === 'busy' && <ErrorIcon as={PhoneOff}></ErrorIcon>}
-                    </Cell>
-                    <Cell>{item.phoneNumber}</Cell>
-                    <Cell>{format(new Date(item.callDate), 'dd.MM.yyyy hh:mm:ss')}</Cell>
-                    <Cell>{item.curator}</Cell>
-                    <Cell>{item.duration ? ~~(item.duration / 60) : 'N/A'}</Cell>
-                    <Cell>
-                      <PlayIcon></PlayIcon>
-                    </Cell>
-                    <Cell>
-                      {item.status === 'completed' && <Button>Add</Button>}
-                      {item.status === 'no-response' && <Button>Call Back</Button>}
-                      {item.status === 'busy' && <Button>Call Back</Button>}
-                    </Cell>
-                  </Row>
-                  <Spacer key={i}></Spacer>
-                </Fragment>
-              ))}
-          </tbody>
-        </table>
       </div>
     </>
   );
@@ -215,8 +236,30 @@ let Row = styled.tr<Themed>`
   color: ${({ theme }) => theme.colors.textItem};
 `;
 
+let HCell = styled.th<Themed>`
+  ${tw`p-4 sticky text-left`}
+  top: 60px;
+  background: ${(props) => props.theme.colors.bgItem};
+
+  &:first-child {
+    ${tw`rounded-tl rounded-bl`}
+  }
+
+  &:last-child {
+    ${tw`rounded-tr rounded-br`}
+  }
+`;
+
 let Cell = styled.td`
-  ${tw`py-4 px-0`}
+  ${tw`py-4 px-4`}
+
+  &:first-child {
+    ${tw`rounded-tl rounded-bl`}
+  }
+
+  &:last-child {
+    ${tw`rounded-tr rounded-br`}
+  }
 `;
 
 let Button = styled.button<Themed>`
